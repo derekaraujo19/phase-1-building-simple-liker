15aea5350246acae89e130614a7934094546fdff
Derek Araujo <derekaraujo19@gmail.com>
2021-09-21 13:36:38 -0400

    Assignment Complete

--- main.js
+++ main.js
@@ -4,7 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// create event listener on click on heart
 
+const hearts = document.getElementsByClassName("like-glyph");
+for (i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener('click', handleHeart);
+}
+
+function handleHeart(e) {
+  // debugger;
+  let heart = e.target;
+
+  mimicServerCall()
+    .then(function(){
+      if (heart.innerText === EMPTY_HEART) {
+        // change heart to full when clicked
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart")
+      } else {
+        // changing heart back to empty - change class name to empty string
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch(() => {
+      const error = document.getElementById('modal');
+      // console.log(error.childNodes);
+      error.classList.remove('hidden');
+      error.childNodes[3].innerText = "Random server error. Try again."
+      setTimeout(function(){
+        error.classList.add('hidden');
+       }, 3000);
+    })
+  }
 
 
 //------------------------------------------------------------------------------
